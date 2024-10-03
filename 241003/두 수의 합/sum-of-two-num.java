4a9d0c4193ce8776d93075cdd0a55929cfaba33c
hseong3243 <48748265+hseong3243@users.noreply.github.com>
2024-10-03 12:19:53 +0900

    Title: 두 수의 합, Time: 497ms, Memory: 26MB, Status: Passed - CodeTree

--- 241003/두 수의 합/sum-of-two-num.java
+++ 241003/두 수의 합/sum-of-two-num.java
@@ -17,34 +17,18 @@ public class Main {
 
     private void solution(int n, int k, int[] arr) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i: arr) {
-            int count = map.getOrDefault(i, 0);
-            map.put(i, count + 1);
-        }
 
         int result = 0;
-        Arrays.sort(arr);
-        for(int i=0; i<n-1; i++) {
-            if(arr[i] * 2 == k) {
-                result += map.get(arr[i]) - 1;
-                map.put(arr[i], map.get(arr[i]) - 1);
-                continue;
+        for(int i=0; i<n; i++) {
+            int target = k - arr[i];
+            if(map.containsKey(target)) {
+                result += map.get(target);
             }
 
-            int min = i+1;
-            int max = n-1;
-            while(min <= max) {
-                int idx = (min + max) / 2;
-                int mid = arr[idx];
-                int sum = mid + arr[i];
-                if(sum > k) {
-                    max = idx - 1;
-                } else if(sum < k) {
-                    min = idx + 1;
-                } else {
-                    result += map.get(mid);
-                    break;
-                }
+            if(map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
             }
         }
         System.out.println(result);
